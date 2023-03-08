ddfd7794eb5e9d9b144bcf506c50f8d9efd0e75a
ShanHu <shanhu94@yeah.net>
2023-03-08 22:47:29 +0800

    添加本地文件

--- lib/config/network.dart
+++ lib/config/network.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/services.dart';
+
 class NetworkPath {
   final String path;
   const NetworkPath(this.path);
@@ -16,12 +18,21 @@ class NetworkPath {
 class Network {
   final client = HttpClient();
 
+  /// debug为true时读取本地文件
+  /// debug为false时, 读取网络配置文件, 需要保证数据结构一致
+  final debug = true;
+
   Uri uri(String path) {
     return Uri.parse('http://127.0.0.1:80/$path');
   }
 
   Future<dynamic> get(NetworkPath path) async {
-    File(path.path);
+    if (debug) {
+      final text = await rootBundle.loadString(path.path);
+      final json = jsonDecode(text);
+      return json;
+    }
+
     final request = await client.getUrl(uri(path.path));
     final response = await request.close();
     if (response.statusCode == 200) {
@@ -35,13 +46,23 @@ class Network {
 
   Future<dynamic> post(NetworkPath path,
       {Map<String, dynamic>? parameter}) async {
+    if (debug) {
+      final text = await rootBundle.loadString(path.path);
+      final json = jsonDecode(text);
+      return json;
+    }
+
     final request = await client.postUrl(uri(path.path));
     if (parameter != null) {
       request.write(jsonEncode(parameter));
     }
     final response = await request.close();
-    final text = await response.transform(utf8.decoder).join();
-    final json = jsonDecode(text);
-    return json;
+    if (response.statusCode == 200) {
+      final text = await response.transform(utf8.decoder).join();
+      final json = jsonDecode(text);
+      return json;
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
   }
 }
